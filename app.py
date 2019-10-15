36e36549ae50f9c058e8507c413989360277cffa
solaaahhh <mnsolahuddin@gmail.com>
2019-10-15 10:23:29 +0800

    Fixed

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import json
+import os
+
+UPLOAD_FOLDER = './files'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -11,8 +15,11 @@ def index():
 def upload():
     # as usual, use request to get the uploaded file, 'file' is because the input name="file"
     file = request.files['file']
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
     # open the file, using the filename, and load it as json
-    data = json.load(open(file.filename))
+    data = json.load(open('./files/'+file.filename))
     # return json as string
     return json.dumps(data)
 
