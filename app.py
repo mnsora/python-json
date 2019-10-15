7e636ca6d83adff02b01d8a5f746549d2ab839b5
yunsyazwani <37645665+yunsyazwani@users.noreply.github.com>
2019-10-15 15:25:50 +0800

    Update app.py
    this

--- app.py
+++ app.py
@@ -25,3 +25,22 @@ def upload():
 
 if __name__ == '__main__':
    app.run()
+   
+   
+   @app.route('/db/uploadjson')
+def uploadjson():
+    alldatas = get_all_data()
+    return render_template('upload.html',alldatas=alldatas)
+
+@app.route('/upload', methods=['POST'])
+def upload():  
+
+     # as usual, use request to get the uploaded file, 'file' is because the input name="file"
+    file = request.files['file']
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    # open the file, using the filename, and load it as json
+    data = json.load(open('./files/'+file.filename))
+    # return json as string
+    return json.dumps(data)
